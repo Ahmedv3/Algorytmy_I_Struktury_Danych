85731a5a041142a7d1c91c0dff89b9e84a82d966
Jakub Czarnecki <ahmed122.90@o2.pl>
2019-05-14 22:06:33 +0200

    Add files via upload

--- lista6/zad1.cpp
+++ lista6/zad1.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
- 
+
 unsigned int h(string s)
 {
   unsigned int h,i;
- 
+
   h = 0;
   for(i = 0; i < s.length(); i++)
-    h = 31 * h + s[i] - 65;
+    h = h + s[i];
   return h % 10;
 }
- 
- 
+
+
 using namespace std;
- 
+
 int main(){
- 
+
 }
